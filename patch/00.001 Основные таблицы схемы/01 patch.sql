38adee345229b804cc325d767028a00502a6ee52
Artem-7 <amuhachev@gmail.com>
2017-02-12 23:14:13 +0700

    1. Переименование объектов. Изменил префиксы с MW на RS 2. Патч сейчас в двух вариантах. Первая версия накатывает схему с нуля, вторая версия - переименовывает существующий объекты. Выбираем то, что нравится

--- patch/00.001 Основные таблицы схемы/01 patch.sql
+++ patch/00.001 Основные таблицы схемы/01 patch.sql
@@ -1,7 +1,7 @@
-@@ddl\mw_passports.tab
-@@ddl\mw_stops.tab
-@@ddl\mw_stop_points.tab
-@@ddl\mw_ways.tab
-@@ddl\mw_way_points.tab
-@@ddl\mw_routes.tab
-@@ddl\mw_tariffs.tab
+@@ddl\rs_passports.tab
+@@ddl\rs_stops.tab
+@@ddl\rs_stop_points.tab
+@@ddl\rs_ways.tab
+@@ddl\rs_way_points.tab
+@@ddl\rs_routes.tab
+@@ddl\rs_tariffs.tab
